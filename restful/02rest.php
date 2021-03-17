1bea8cc3d4661768e7d8e03577ddcb7a117a0d30
far13579 <6306021421201@fitm.kmutnb.ac.th>
2021-03-17 18:09:55 +0700

    m

--- restful/02rest.php
+++ restful/02rest.php
@@ -1,19 +1,23 @@
+
 <?php
-    include_once "01dbcontrol.php";
-    include_once "until.php";
+    include_once("01dbcontrol.php");
+    include_once("util.php");
     $debug_mode = false;
-    if($_SERVER['REQUEST_METHOD']=='GET'){
-        debug_text("for GET Method", $debug_mode);
-        echo json_encode(show_data($debug_mode));
-    }else if(_$SERVER['REQUEST_METHOD']=='POST'){
+    
+    if($_SERVER['REQUEST_METHOD'] == 'GET'){ # select
+        debug_text("for GET Method" ,$debug_mode); 
+        show_data($debug_mode);
+    }else if($_SERVER['REQUEST_METHOD'] == 'POST'){ # insert , update , delete
         debug_text("for POST Method", $debug_mode);
     }else{
-        debug_text("Error Unknow this Request", $debug_mode);
+        debug_text("Error Unknow this Request" ,$debug_mode);
         http_response_code(405);
     }
-    function show_data(){
-        $mydb = new db ("root","","sec2",$debug_mode);
-        echo json_encode($mydb->query("select * from customer"));
+
+    function show_data($debug_mode){
+        $mydb = new db("sucubus","","sec2",$debug_mode);
+        echo json_encode($mydb->query("SELECT * FROM customer"));
         $mydb->close();
+        
     }
 ?>
--- restful/03restful.php
+++ restful/03restful.php
@@ -1,16 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body onload="loadContent()">
-    <script >
-        function loadContent(){
 
+<body onload="loadContent()">
+    <div id="out"> </div>
+    <script>
+        function loadContent() {
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                console.log(this.readyState + "" + this.status);
+                if (this.readyState == 4 && this.status == 200) {
+                    console.log(this.responseText);
+                    data = JSON.parse(this.responseText);
+                    out = document.getElementbyId("out");
+                    console.log (data.length);
+                    for (let i = 0; i < data.length; i++) {
+                        for(inf in data[i]){
+                             text += data[i][inf]+" ";
+                        }
+                       text+="\n";
+                    }
+                    out.innerHTML = text;
+                }
+            }
+            xhttp.open("GET", "02rest.php", true);
+            xhttp.send();
         }
     </script>
 </body>
+
 </html>

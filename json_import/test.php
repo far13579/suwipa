3abe64d05035d05144d0e8e3dfe9899d2bd44503
far13579 <6306021421201@fitm.kmutnb.ac.th>
2021-03-17 19:24:30 +0700

    t

--- json_import/test.php
+++ json_import/test.php
@@ -34,7 +34,17 @@
             return jSonEX;
         }
         function load_title(){
-            alert("YEAR Change");
+            alert("Year Change");
+            // alert("Total Year "+movie_year.size);
+            var doc = document.getElementById("Movie_Title");
+            var doc = document.getElementById("sel_year");
+            alert(y.value);
+            // const ref_year = movie_year.values();
+            // for ( y = 0; y<movie_year.size;y++) {
+            //     var option = document.createElement("option");
+            //     option.text = ref_year.next().value;
+            //     doc.add(option);  
+            // }
         }
     </script>
 </body>

dee561cba114490e2b31eada128aeaf37aae7fa7
far13579 <6306021421201@fitm.kmutnb.ac.th>
2021-03-19 10:51:33 +0700

    m

--- restful/02rest.php
+++ restful/02rest.php
@@ -5,25 +5,31 @@
     $debug_mode = false;
     if($_SERVER['REQUEST_METHOD'] == 'GET'){ # select
         echo json_encode(show_data($debug_mode));
-        // debug_text("for GET Method" ,$debug_mode); 
         show_data($debug_mode);
     }else if($_SERVER['REQUEST_METHOD'] == 'POST'){ # insert , update , delete
         debug_text("POST may be support next time <br>",$debug_mode);
-        $message = '{"Status":'+print_r($_POST)+'}';
-        echo json_encode($message);
-        // debug_text("for POST Method", $debug_mode);
+        if (isset($_POST['u_id'])&& isset($_POST['u_name'])) {
+            $u_id = $_POST['u_id'];
+            $u_name = $_POST['u_name'];
+            insert_newdata($u_id,$u_name,$debug_mode);
+        }
+        // $message = '{"Status":'+print_r($_POST)+'}';
+        // echo json_encode($message);
     }else{
-        // debug_text("Error Unknow this Request" ,$debug_mode);
         http_response_code(405);
     }
 
     function show_data($debug_mode){
         $mydb = new db("root","","sec2",$debug_mode);
-        // echo json_encode($mydb->query("SELECT * FROM customer"));
         $data = $mydb->query("select * from user");
         return $data;
-
-        $mydb->close();
+        // $mydb->close();
         
+    }
+    function insert_newdata($u_id,$u_name,$debug_mode){
+        $mydb = new db("root","","sec2",$debug_mode);
+        $data = $mydb->query("instert.....");
+        show_data($debug_mode);
+        return $data;
     }
 ?>

143d6430c5f40699c0afbfb5843e031f72a07383
far13579 <6306021421201@fitm.kmutnb.ac.th>
2021-03-19 10:33:28 +0700

    h

--- restful/02rest.php
+++ restful/02rest.php
@@ -3,20 +3,26 @@
     include_once("01dbcontrol.php");
     include_once("util.php");
     $debug_mode = false;
-    
     if($_SERVER['REQUEST_METHOD'] == 'GET'){ # select
-        debug_text("for GET Method" ,$debug_mode); 
+        echo json_encode(show_data($debug_mode));
+        // debug_text("for GET Method" ,$debug_mode); 
         show_data($debug_mode);
     }else if($_SERVER['REQUEST_METHOD'] == 'POST'){ # insert , update , delete
-        debug_text("for POST Method", $debug_mode);
+        debug_text("POST may be support next time <br>",$debug_mode);
+        $message = '{"Status":'+print_r($_POST)+'}';
+        echo json_encode($message);
+        // debug_text("for POST Method", $debug_mode);
     }else{
-        debug_text("Error Unknow this Request" ,$debug_mode);
+        // debug_text("Error Unknow this Request" ,$debug_mode);
         http_response_code(405);
     }
 
     function show_data($debug_mode){
-        $mydb = new db("sucubus","","sec2",$debug_mode);
-        echo json_encode($mydb->query("SELECT * FROM customer"));
+        $mydb = new db("root","","sec2",$debug_mode);
+        // echo json_encode($mydb->query("SELECT * FROM customer"));
+        $data = $mydb->query("select * from user");
+        return $data;
+
         $mydb->close();
         
     }
--- restful/03restful.php
+++ restful/03restful.php
@@ -14,23 +14,40 @@
         function loadContent() {
             let xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
-                console.log(this.readyState + ", " + this.status);
-                if (this.readyState==4 && this.status==200) {
-                    console.log(this.responseText);
-                    data = JSON.parse(this.responseText);
-                    out = document.getElementById("out");
-                    console.log(data.length);
-                    for (i=0;i<data.length;i++) {
-                        for(inf in data[i]){
-                             text += "<td>"+data[i][inf]+"</td>";
+                console.log(this.readyState);
+                // console.log(this.readyState + ", " + this.status);
+                if (this.readyState == 4 && this.status == 200) {
+                    alert(this.responseText);
+                    // console.log(this.responseText);
+                    my = JSON.parse(this.responseText);
+                    m = document.getElementById("out");
+                    text = "<table border ='1'>";
+                    for (i = 0; i < my.length; i++) {
+                        for (inf in my[i]) {
+                            text += "<td>" + my[i][inf] + "</td>";
                         }
-                       text+="\n";
+                        text += "</tr>";
                     }
-                    out.innerHTML = text;
+                    text += "</table>";
+                    alert(text);
+                    out.innerHTML += text;
                 }
             }
             xhttp.open("GET", "02rest.php", true);
             xhttp.send();
+        }
+        function add_new() {
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function(){
+                if(this.readyState == 4 && this.status == 200){
+                    alert(this.responseText);
+                }
+            }
+            xhttp.open("POST","02rest.php",true);
+            xhttp.setRequestHeader("content-type","application/x-www-from-urlrncodeed");
+            u_id = document.getElementById("u_id");
+            u_name = document.getElementById("u_id");
+            xhttp.send("u_id="+u_id.value+"&u_name="+u_name.value);
         }
     </script>
 </body>
